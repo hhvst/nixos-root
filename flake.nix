90e5e8ff85f9181770c28de1b1ae5906284e49bf
HC <hc@vst.io>
2026-03-22 13:41:36 +0000

    set nixos version to 25.11

--- flake.nix
+++ flake.nix
@@ -1,8 +1,8 @@
 {
-  description = "My NixOS configurations"
+  description = "My NixOS configurations";
 
-  inputs{
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     disko = {
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
